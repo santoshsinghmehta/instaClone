8f1bdbde1bb239e484b5ad64af8b4f6b32316b58
santoshsinghmehta <santoshmehta1996@gmail.com>
2018-05-13 04:10:01 +0530

    implementation in code

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,64 +1,60 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+from django.shortcuts import render, redirect
+from forms import SignUpForm, LoginForm, PostForm, LikeForm, CommentForm
+from models import UserModel, SessionToken, PostModel, LikeModel, CommentModel
+from django.contrib.auth.hashers import make_password, check_password
 from datetime import timedelta
-from time import timezone
-
-from django.shortcuts import render,redirect
-from forms import SignUpForm,LoginForm,PostForm,LikeForm
-from models import UserModel,SessionToken,PostModel,LikeModel
-from django.contrib.auth.hashers import make_password,check_password
+from django.utils import timezone
 from myproject.settings import BASE_DIR
+
 from imgurpython import ImgurClient
 
-YOUR_CLIENT_ID = 'f5119726c4ba0f9'
-YOUR_CLIENT_SECRET = '466ab27d37ccc8506b870ea2ad5a6c757738bf9c'
 
 # Create your views here.
+
 def signup_view(request):
-    if request.method =='POST':
+    if request.method == "POST":
         form = SignUpForm(request.POST)
         if form.is_valid():
             username = form.cleaned_data['username']
             name = form.cleaned_data['name']
             email = form.cleaned_data['email']
             password = form.cleaned_data['password']
+            # saving data to DB
             user = UserModel(name=name, password=make_password(password), email=email, username=username)
             user.save()
             return render(request, 'success.html')
-
+            # return redirect('login/')
     else:
         form = SignUpForm()
-    return render(request, 'index.html',{'form':form})
+
+    return render(request, 'index.html', {'form': form})
 
 
 def login_view(request):
-    dict = {}
+    response_data = {}
     if request.method == "POST":
         form = LoginForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            #sql query
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
             user = UserModel.objects.filter(username=username).first()
+
             if user:
-                if not check_password(password, user.password):
-                    dict['messages']='invalid password please try again'
-                else:
+                if check_password(password, user.password):
                     token = SessionToken(user=user)
                     token.create_token()
                     token.save()
-                    response = redirect('/feed/')
+                    response = redirect('feed/')
                     response.set_cookie(key='session_token', value=token.session_token)
                     return response
-    else:
+                else:
+                    response_data['message'] = 'Incorrect Password! Please try again!'
+
+    elif request.method == 'GET':
         form = LoginForm()
 
-    dict['form']=form
-    return render(request, 'login.html', dict)
-
-
-
+    response_data['form'] = form
+    return render(request, 'login.html', response_data)
 
 
 def post_view(request):
@@ -73,26 +69,35 @@ def post_view(request):
                 post = PostModel(user=user, image=image, caption=caption)
                 post.save()
 
-                path = str(BASE_DIR + '/' +post.image.url)
+                path = str(BASE_DIR + post.image.url)
 
                 client = ImgurClient(YOUR_CLIENT_ID, YOUR_CLIENT_SECRET)
-                post.image_url = client.upload_from_path(path,anon=True)['link']
+                post.image_url = client.upload_from_path(path, anon=True)['link']
                 post.save()
 
                 return redirect('/feed/')
 
         else:
             form = PostForm()
-        return render(request, 'post.html', {'form' : form})
+        return render(request, 'post.html', {'form': form})
     else:
         return redirect('/login/')
+
 
 def feed_view(request):
     user = check_validation(request)
     if user:
-            posts = PostModel.objects.all().order_by('created_on')
-            return render(request, 'feed.html', {'posts': posts})
+
+        posts = PostModel.objects.all().order_by('created_on')
+
+        for post in posts:
+            existing_like = LikeModel.objects.filter(post_id=post.id, user=user).first()
+            if existing_like:
+                post.has_liked = True
+
+        return render(request, 'feed.html', {'posts': posts})
     else:
+
         return redirect('/login/')
 
 
@@ -109,17 +114,32 @@ def like_view(request):
                 existing_like.delete()
             return redirect('/feed/')
     else:
+        return redirect('/login/')
+
+
+def comment_view(request):
+    user = check_validation(request)
+    if user and request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            post_id = form.cleaned_data.get('post').id
+            comment_text = form.cleaned_data.get('comment_text')
+            comment = CommentModel.objects.create(user=user, post_id=post_id, comment_text=comment_text)
+            comment.save()
+            return redirect('/feed/')
+        else:
+            return redirect('/feed/')
+    else:
         return redirect('/login')
 
 
-
-#For validating the session
+# For validating the session
 def check_validation(request):
     if request.COOKIES.get('session_token'):
         session = SessionToken.objects.filter(session_token=request.COOKIES.get('session_token')).first()
         if session:
-            #time_to_live = session.created_on + timedelta(day=1)
-            if True:
+            time_to_live = session.created_on + timedelta(days=1)
+            if time_to_live > timezone.now():
                 return session.user
     else:
         return None
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from myapp.views import signup_view,login_view,post_view,feed_view,like_view
+from myapp.views import signup_view,login_view,post_view,feed_view,like_view,comment_view
 
 urlpatterns = [
+    url('comment/', comment_view),
     url('like/', like_view),
     url('post/', post_view),
     url('feed/', feed_view),
